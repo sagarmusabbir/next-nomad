106b7354a59598de34e9ba51b08e841ad1a8ac93
sagarmusabbir <sagarmusabbir@gmail.com>
2024-03-23 05:20:16 +0600

    fix-lusitana-typo

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,12 @@
-import { Inter, Montserrat } from 'next/font/google';
+import { Inter, Lusitana, Montserrat } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const montserrat = Montserrat({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
